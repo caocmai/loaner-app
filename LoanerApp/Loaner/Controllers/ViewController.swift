f1cf04f25f7bbbc25ca2739b2522b3efe0d8e765
Cao Mai <caomai@rocketmail.com>
2020-06-15 17:46:52 -0700

    implemented persistent container

--- LoanerApp/Loaner/Controllers/ViewController.swift
+++ LoanerApp/Loaner/Controllers/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     var items: [Item] = []
+    var store: ItemStore!
+
     
     @IBOutlet weak var collectionView: UICollectionView!
     
